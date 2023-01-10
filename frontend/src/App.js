865b63f4dfb445d5e53531eb475008e387ee655b
JenkaCool <cool.diatel@yandex.ru>
2023-01-11 00:30:19 +0300

    change main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 function App() {
+  const [taskItems, setTaskItems] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3010/api/task-items')
+      .then((res) => res.json())
+      .then((result) => setTaskItems(result.data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {taskItems.map((item) => (
+        <Form.Group key={item.id} className="app__task-item">
+          <Form.Check type="checkbox" checked={item.done} />
+          <Form.Control type="text" value={item.text} />
+
+        </Form.Group>
+      ))}
     </div>
   );
 }
